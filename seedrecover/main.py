d947fd235a1d34a74911468a160c46ba62afc44f
Benjamin Braatz <bb@bbraatz.eu>
2022-01-31 00:34:30 +0100

    Added length and missing word handling in main.py.

--- seedrecover/main.py
+++ seedrecover/main.py
@@ -4,6 +4,8 @@ import sys
 
 from seedrecover.wordlist import Wordlist
 
+from typing import Optional
+
 
 def parse_args() -> argparse.Namespace:
     """Parse the command line arguments."""
@@ -29,14 +31,55 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def possible_seed_words(wordlist: Wordlist, known: list[str],
+                        similar: int) -> list[list[str]]:
+    """Determine possible seed words from given known words."""
+    seed = []
+    for word in known:
+        if not wordlist.contains(word):
+            print(f"'{word}' not in wordlist!", file=sys.stderr)
+        words = wordlist.get_words(word, similar)
+        print(f"{word} => {', '.join(words)}", file=sys.stderr)
+        seed.append(words)
+    return seed
+
+
+def compute_length(length: Optional[int], known: int) -> int:
+    """Determine length of seed phrase to search for."""
+    if not length:
+        print("Length not set. Using smallest length for given phrase.",
+              file=sys.stderr)
+        length = known
+        if known % 3:
+            length += 3 - known % 3
+    if length % 3 != 0:
+        print("Length is not a multiple of 3!", file=sys.stderr)
+        exit(1)
+    if known > length:
+        print("More known words than length given!", file=sys.stderr)
+        exit(1)
+    print(f"{length - known} of {length} words missing.", file=sys.stderr)
+    return length
+
+
+def determine_missing_positions(given: list[int], length: int,
+                                missing: int) -> list[int]:
+    """Check or determine positions for missing words."""
+    missing_positions = [i-1 for i in given]
+    if not missing_positions:
+        missing_positions = list(range(length))
+    if len(missing_positions) < missing:
+        print(f"Only {len(missing_positions)} positions given"
+              " for missing words!", file=sys.stderr)
+        exit(1)
+    return missing_positions
+
+
 def main() -> None:
     """Execute the main control flow of the seedrecover script."""
     args = parse_args()
     wordlist = Wordlist(args.wordlist)
-    seed = []
-    for word in args.seed:
-        if not wordlist.contains(word):
-            print(f"'{word}' not in wordlist!", file=sys.stderr)
-        words = wordlist.get_words(word, args.similar)
-        print(f"{word} => {', '.join(words)}", file=sys.stderr)
-        seed.append(words)
+    seed = possible_seed_words(wordlist, args.seed, args.similar)
+    length = compute_length(args.length, len(seed))
+    missing_positions = determine_missing_positions(args.missing, length,
+                                                    length - len(seed))
